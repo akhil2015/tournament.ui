25b70907121a3ce3aaf84d414ae6ab6f51ff343f
Akhil Bharti <akhil.bh96@gmail.com>
2023-05-05 01:29:48 +0530

    added scorecard

---
+++ src/components/ScoreCard.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+type Props = {};
+
+export default function ScoreCard({}: Props) {
+  return (
+    <div>
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Scorecard</h3>
+          <div className="overflow-x-auto">
+            <table className="table w-full">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Address</th>
+                  <th>Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                <tr>
+                  <th>1</th>
+                  <td>Cy Ganderton</td>
+                  <td>40</td>
+                </tr>
+                {/* row 2 */}
+                <tr>
+                  <th>2</th>
+                  <td>Hart Hagerty</td>
+                  <td>25</td>
+                </tr>
+                {/* row 3 */}
+                <tr>
+                  <th>3</th>
+                  <td>Brice Swyre</td>
+                  <td>12</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
